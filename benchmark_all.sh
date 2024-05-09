67befe62ea6814800481f791aa031c141733d8ab
PatchOfScotland <d.marchant@ed-alumni.net>
2024-05-09 15:15:19 +0200

    added missing arg in benchmark_all

--- benchmark_all.sh
+++ benchmark_all.sh
@@ -5,8 +5,8 @@ REPEATS=$1
 make clean
 mkdir -p build
 
-./benchmark_map.sh
-./benchmark_reduce.sh
-./benchmark_matmul.sh
+./benchmark_map.sh $REPEATS
+./benchmark_reduce.sh $REPEATS
+./benchmark_matmul.sh $REPEATS
 
 python3 assemble_graph.py
